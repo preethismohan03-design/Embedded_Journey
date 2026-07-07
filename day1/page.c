1f23389e70f1bb311e5bc55694343da6e9ef766c
preethi_mohan <preethismohan03@gmail.com>
2026-07-07 15:47:07 +0530

    Day 1 - Added basic C programs

---
+++ day1/page.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    int age = 25;
+    printf("%d", age);
+}
---
+++ day1/pmul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Preethi";
+    int age = 26;
+    float height = 149.5;
+
+    printf("Name   : %s\n", name);
+    printf("Age    : %d\n", age);
+    printf("Height : %.1f cm\n", height);
+
+    return 0;
+}
