b674649fcf0db2bc6ae7c777ee9e29a617952c44
preethi_mohan <preethismohan03@gmail.com>
2026-07-08 12:50:31 +0530

    day2 adding two number

---
+++ day2/add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,d,c ;
+    printf("enter a = ");
+    scanf("%d",&a);
+    printf("enter  d = ");
+    scanf("%d",&d);
+
+    c = a + d ;
+    printf("c=%d", c);
+    return 0 ;
+}
