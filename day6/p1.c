3fbfaa5ae5ca6e8f186537d124f4a787a13eae34
preethi_mohan <preethismohan03@gmail.com>
2026-07-13 13:04:39 +0530

    Print Array Elements

---
+++ day6/p1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int  i ;
+    int  ary[6] = { 12,13,14,15,16,11};
+
+    for (i = 0; i < 6 ; i++)
+    {
+        printf("%d\n",ary[i]);
+    }
+    return 0 ;
+  
+}
