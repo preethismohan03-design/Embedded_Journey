bbb7f8c8f3b6b00bb99f10e43876a1b2764578e8
preethi_mohan <preethismohan03@gmail.com>
2026-07-14 15:05:05 +0530

    Print Star Line

---
+++ day7/p2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ void line ();
+ int main ()
+ {
+
+    line();
+    printf("curse\n");
+    line();
+     return 0;
+
+
+ }
+
+ void line ()
+ {
+    printf("****************\n");
+ }
